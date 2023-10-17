f91cc9bc002d97e1b06d67388d37b7c4dc7ab0ad
Mahmoud <mahmoud.nb@gmail.com>
2023-10-17 11:19:56 +0200

    Add and use Internationalization with next-intl

---
+++ app/[locale]/page.tsx
@@ -0,0 +1,10 @@
+import {useTranslations} from 'next-intl'
+
+export default function Home() {
+  const t = useTranslations('Index')
+  return (
+    <div>
+      <h2>{t('title')}</h2>
+    </div>
+  )
+}
