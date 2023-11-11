aa45a6574ad271e34173077817d35e1d3236960f
medunes <contact@medunes.net>
2023-11-11 16:02:58 +0100

    add docker local setup (dev)

--- next.config.js
+++ next.config.js
@@ -1,11 +1,27 @@
 const withNextIntl = require('next-intl/plugin')(
-  // This is the default (also the `src` folder is supported out of the box)
-  './i18n.ts'
+    // This is the default (also the `src` folder is supported out of the box)
+    './i18n.ts'
 );
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = withNextIntl({
-  ...nextConfig
-});
+const path = require('path')
+const moduleExports = {
+    ...nextConfig,
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+    },
+    reactStrictMode: false,
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ["@svgr/webpack"]
+        });
+        return config;
+    },
+    // Exclude "/favicon.ico" from prerendering
+    trailingSlash: true // Ensure this line is present
+}
+
+module.exports = withNextIntl(moduleExports)
