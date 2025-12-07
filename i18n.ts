a91c63e1ca48736b38c71c3112732693cc755684
Mahmoud <mahmoud.nb@gmail.com>
2025-12-07 11:17:22 +0100

    Refactor code structure for improved readability and maintainability

--- i18n.ts
+++ i18n.ts
@@ -1,5 +1,6 @@
 import {getRequestConfig} from 'next-intl/server';
  
-export default getRequestConfig(async ({locale}) => ({
+export default getRequestConfig(async ({locale = 'en'}) => ({
+  locale,
   messages: (await import(`./messages/${locale}.json`)).default
 }));
